f176a589a8fd3bec4f093754a0587e695bce0e99
mphoenix7 <zarotikh@gmail.com>
2025-06-26 16:03:41 +0000

    change noti

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import Header from '@/components/navbar/navbar'
 import SideBar from '@/components/side-bar'
 import { Role } from '@/types/roles'
 import { redirect } from 'next/navigation'
+import { getSession } from '@/lib/auth'
 
 export const metadata: Metadata = {
   title: 'CMGA Delivery',
@@ -19,17 +20,23 @@ export default async function RootLayout({
   children: React.ReactNode
 }) {
   const user = await getCurrentUser()
+  const { token } = await getSession()
+  console.log(token, "token layout")
+
+
+
+
 
   return (
     <html lang="fr">
       <body>
         <div className="flex min-h-screen">
           {/* Sidebar */}
-          {user && user.role === Role.ADMIN && <SideBar />}
+          {token && user && user.role === Role.ADMIN && <SideBar />}
           {/* Main content */}
           <div className="flex-1">
             {/* Header */}
-            {user && <header className="border-b bg-white shadow-sm sticky top-0 z-40">
+            {token && user && <header className="border-b bg-white shadow-sm sticky top-0 z-40">
               <Header user={user} />
             </header>}
             <main className="p-4 md:p-6 bg-gray-50 min-h-[calc(100vh-64px)]">
--- lib/auth.ts
+++ lib/auth.ts
@@ -4,20 +4,20 @@ import { cookies } from "next/headers";
 
 export async function getSession() {
   const cookieStore = await cookies()
-  const token = cookieStore.get("token")?.value;
+  const session = JSON.parse(cookieStore.get("accessToken")?.value || "{}")
 
-  if (!token) return null;
+  if (!session) return null;
 
-  axios.defaults.headers.common["Authorization"] = `Bearer ${token}`
-  return token;
+  // axios.defaults.headers.common["Authorization"] = `Bearer ${session.token}`
+  return session;
 }
 
 export async function getUser() {
   const cookieStore = await cookies()
   const userId = cookieStore.get("userId")?.value;
-    
+
   if (!userId) return null;
-    
+
   try {
     const user = await axios.get(`${USERS_API}/${userId}`)
     if (!user) return null;
