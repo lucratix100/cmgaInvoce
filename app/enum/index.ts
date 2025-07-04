56e341029efe1e3f37afe51e63e5be0d1f21f304
lucratix100 <saliou.0297@gmail.com>
2025-07-04 19:15:37 +0000

    fix conflit

---
+++ app/enum/index.ts
@@ -0,0 +1,31 @@
+export enum Role {
+  ADMIN = 'ADMIN',
+  MAGASINIER = 'MAGASINIER',
+  CHEF_DEPOT = 'CHEF DEPOT',
+  RECOUVREMENT = 'RECOUVREMENT',
+  CONTROLEUR = 'CONTROLEUR',
+  SUPERVISEUR_MAGASIN = 'SUPERVISEUR_MAGASIN',
+}
+
+export enum InvoiceStatus {
+  NON_RECEPTIONNEE = 'non réceptionnée',
+  EN_ATTENTE = 'en attente de livraison',
+  EN_COURS = 'en cours de livraison',
+  LIVREE = 'livrée',
+}
+
+export enum InvoicePaymentStatus {
+  NON_PAYE = 'non payé',
+  PAIEMENT_PARTIEL = 'paiement partiel',
+  PAYE = 'payé',
+  // ANNULE = 'annulé',    
+}
+
+export enum PaymentMethod {
+  ESPECE = 'Espece',
+  CHEQUE = 'Cheque',
+  VIREMENT = 'Virement',
+  MOBILE_MONEY = 'Mobile Money',
+  RETOUR = 'Retour',
+  OD = 'OD',
+}
